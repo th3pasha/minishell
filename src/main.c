2e29432052da0e8c71a48976e226b59dd5bf7778
Ayoub <elpasha117@gmail.com>
2024-01-31 15:40:20 +0100

    adding clean, trim, and move func

--- src/main.c
+++ src/main.c
@@ -1,3 +1,10 @@
+/*
+  A basic shell implementation.
+
+  Ayoub AZIZI, UPJV, 2023
+  
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,20 +12,28 @@
 #include "parser.h"
 #include "cmd.h"
 
+void terminal(){
+  char *cwd; 
+  char *usr;
+  getcwd(cwd, MAX_LINE_SIZE);
+  getlogin_r(usr, MAX_LINE_SIZE);
+
+  printf("%s@%s$ ", usr, cwd);
+}
+
 int main(int argc, char* argv[]) {
   char cmdline[MAX_LINE_SIZE]; 
   char* cmdtoks[MAX_CMD_SIZE]; 
   cmd_t cmds[MAX_CMD_SIZE];
   cmd_t* current;
   
+  char* cwd;
+
   while (1) {
-    
-    printf("$ ");
-    
+    terminal();
     
     if (fgets(cmdline, MAX_LINE_SIZE, stdin)==NULL) break;
     cmdline[strlen(cmdline)-1]='\0';
-    
     
     for (current=cmds; current!=NULL; ) {
       
@@ -26,7 +41,6 @@ int main(int argc, char* argv[]) {
       
     }
   }
-  
-  fprintf(stderr, "\nGood bye!\n");
+  fprintf(stderr, "\nExiting ~shell!\n");
   return 0;
 }
--- src/parser.c
+++ src/parser.c
@@ -1,11 +1,63 @@
 #include <stddef.h>
 
-int trim(char* str) {
+void move(char *dest, const char *src, size_t n) {
+    if (dest > src && dest < src + n) {
+        dest += n - 1;
+        src += n - 1;
 
+        while (n--) {
+            *dest-- = *src--;
+        }
+    } else { 
+        while (n--)
+        { 
+            *dest++ = *src++;
+        }
+    }
+}
+
+int trim(char *str)
+{
+    size_t begin = 0;
+    size_t last = strlen(str) - 1;
+
+    clean(str);
+    
+    while (str[begin] == ' '){
+        begin++;
+    }
+    while (str[last] == ' '){
+        last--;
+    }
+
+    move(str, str + begin, last - begin + 1);
+    str[last - begin + 1] = '\0';
+    
+    return 0;
 }
 
 int clean(char* str) {
+ size_t length = strlen(str); 
+    int isSpace = 0; 
+    size_t i, j; 
 
+    for (i = 0, j = i; i < length; i++){
+        if (str[i] == ' ')
+        {
+            if (!isSpace)
+            {
+                str[j++] = ' ';
+                isSpace = 1;
+            }
+        } else {
+            str[j++] = str[i];
+            isSpace = 0;
+        }
+    }
+
+    str[j] = '\0'; 
+
+    return 0;
 }
 
 int separate_s(char* str, char* s, size_t max) {
